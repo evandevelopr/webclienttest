70198f38ab65a906dab7e071c43c04ce3185c15b
kairuz <evan.kairuz@gmail.com>
2023-06-14 12:53:53 -0400

    Moved to uvu test framework.

--- tests/lib2.test.js
+++ tests/lib2.test.js
@@ -1,8 +1,11 @@
 import {sub, div} from "../lib2.js";
-
+import { test } from 'uvu';
+import * as assert from 'uvu/assert';
 
 
 test('should subtract and divide', () => {
-  expect(sub(2, 3)).toBe(-1);
-  expect(div(2, 3)).toBe(2 / 3);
-})
+  assert.is(sub(2, 3), -1);
+  assert.is(div(2, 3), 2 / 3);
+});
+
+test.run();
